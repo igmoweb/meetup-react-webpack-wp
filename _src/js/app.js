99f3b11e5f5a0b00977ddda65d64c37de1007b13
Ignacio Cruz <igmoweb@gmail.com>
2017-06-09 11:18:12 +0200

    React App without state

--- _src/js/app.js
+++ _src/js/app.js
@@ -1,21 +1,17 @@
 import { getTodos } from './helpers';
 import styles from '../scss/style.scss';
 import Fetcher from './Fetcher';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let ul = document.createElement( 'ul' );
-ul.classList.add( 'todos' );
+export default class App extends React.Component {
+    render() {
+        return <ul className="todos">
+            <li className="todo-item">One todo</li>
+            <li className="todo-item">Another todo</li>
+        </ul>;
+    }
+}
 
-let app = document.getElementById( 'app' );
 
-Fetcher.getTodos()
-    .then( ( response ) => {
-        response.map( ( todo ) => {
-            let li = document.createElement( 'li' );
-            li.classList.add( 'todo-item' );
-            li.innerHTML = todo.title.rendered;
-            ul.append( li );
-        });
-
-        app.innerHTML = '';
-        app.append( ul );
-    });
+ReactDOM.render( <App />, document.getElementById( 'app' ) );
