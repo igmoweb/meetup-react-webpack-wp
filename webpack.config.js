3a9662912625e9028c5254486887a15738cc39e8
Ignacio Cruz <igmoweb@gmail.com>
2017-06-09 09:34:28 +0200

    Better syntax

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ {
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                } ]
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
