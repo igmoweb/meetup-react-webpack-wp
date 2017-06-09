1d789d547a9dbd85de1fb03c137c85f7b8ccfb73
Ignacio Cruz <igmoweb@gmail.com>
2017-06-09 09:02:33 +0200

    Sass config

--- _src/js/app.js
+++ _src/js/app.js
@@ -1,12 +1,16 @@
 import { getTodos } from './helpers';
+import styles from '../scss/style.scss';
 
 const todos = getTodos();
 
 let ul = document.createElement( 'ul' );
+ul.classList.add( 'todos' );
+
 let app = document.getElementById( 'app' );
 
 todos.map( ( todo ) => {
    let li = document.createElement( 'li' );
+   li.classList.add( 'todo-item' );
    li.innerHTML = todo.title;
    ul.append( li );
    app.append( ul );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 let path = require('path');
 
 module.exports = {
-    entry: './_src/app.js',
+    entry: './_src/js/app.js',
     output: {
         path: path.resolve( __dirname, 'js' ),
         filename: 'app.js'
@@ -17,6 +17,16 @@ module.exports = {
                         presets: ['es2015']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [ {
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                } ]
             }
         ]
     },
