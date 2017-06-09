e9ef34a97bedc5cdfa136381dd63a7d41a2086bb
Ignacio Cruz <igmoweb@gmail.com>
2017-06-09 09:59:06 +0200

    Show CPT in REST API

--- meetup-plugin.php
+++ meetup-plugin.php
@@ -40,8 +40,8 @@ add_action( 'admin_menu', function() {
 
 add_action( 'init', function() {
 	register_post_type( 'todo', array(
-		'description'          => 'Todos',
-		'labels'               => array(
+		'description'         => 'Todos',
+		'labels'              => array(
 			'name'               => _x( 'Todos', 'post type general name', 'todo' ),
 			'singular_name'      => _x( 'Todo', 'post type singular name', 'todo' ),
 			'menu_name'          => _x( 'Todos', 'admin menu', 'todo' ),
@@ -57,21 +57,22 @@ add_action( 'init', function() {
 			'parent_item_colon'  => __( 'Parent Todo:', 'todo' ),
 			'all_items'          => __( 'All Todos', 'todo' ),
 		),
-		'public'               => false,
-		'hierarchical'         => false,
-		'exclude_from_search'  => true,
-		'publicly_queryable'   => false,
-		'show_ui'              => true,
-		'show_in_menu'         => false,
-		'show_in_nav_menus'    => false,
-		'show_in_admin_bar'    => false,
-		'menu_position'        => null,
-		'menu_icon'            => null,
-		'capability_type'      => 'post',
-		'capabilities'         => array(),
-		'map_meta_cap'         => null,
-		'supports'             => array( 'title' ),
-		'has_archive'          => false,
+		'public'              => false,
+		'hierarchical'        => false,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => false,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => false,
+		'show_in_admin_bar'   => false,
+		'menu_position'       => null,
+		'menu_icon'           => null,
+		'capability_type'     => 'post',
+		'capabilities'        => array(),
+		'map_meta_cap'        => null,
+		'supports'            => array( 'title' ),
+		'has_archive'         => false,
+		'show_in_rest'        => true
 	) );
 });
 
